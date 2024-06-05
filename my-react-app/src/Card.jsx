66f76926cba8d61b6645e12b88ece721ab534666
USER 1995 <userfrom1995@gmail.com>
2024-06-05 11:41:57 +0000

    Second Commit

---
+++ my-react-app/src/Card.jsx
@@ -0,0 +1,76 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const CardContainer = styled.div`
+  width: 300px;
+  border-radius: 10px;
+  overflow: hidden;
+  box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
+  background-color: #fff;
+  margin: 20px;
+  text-align: center;
+`;
+
+const CardImage = styled.img`
+  width: 100%;
+  height: 200px;
+  object-fit: cover;
+`;
+
+const CardContent = styled.div`
+  padding: 20px;
+`;
+
+const CardTitle = styled.h3`
+  margin: 0 0 10px;
+  font-size: 1.5em;
+  color: #333;
+`;
+
+const CardDescription = styled.p`
+  margin: 0;
+  font-size: 1em;
+  color: #666;
+`;
+
+const CardButton = styled.a`
+  display: inline-block;
+  margin-top: 20px;
+  padding: 10px 20px;
+  border-radius: 5px;
+  background-color: #6a11cb;
+  color: #fff;
+  text-decoration: none;
+  font-size: 1em;
+  &:hover {
+    background-color: #2575fc;
+  }
+`;
+
+const image = 
+const title =
+const description =
+
+
+
+function Card(){
+    return(
+        <CardContainer>
+        <CardImage src={image} alt={title} />
+        <CardContent>
+          <CardTitle>{title}</CardTitle>
+          <CardDescription>{description}</CardDescription>
+          <CardButton href={buttonLink}>{buttonText}</CardButton>
+        </CardContent>
+      </CardContainer>
+      
+        
+
+
+
+
+
+    );
+};
+
+export default App
---
+++ my-react-app/src/Footer.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+ // Import CSS file for styling
+
+function Footer() {
+  return (
+    <footer className="footer">
+        
+      
+          <p className="copyright">
+            &copy; {new Date().getFullYear()} Your Website Name. All rights reserved.
+          </p>
+          
+    </footer>
+  );
+};
+
+export default Footer;
